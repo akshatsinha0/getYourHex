a771af88f2141496a0eb1fc0f4563d2612bf24ce
Akshat Sinha <akshatsinhasramhardy@gmail.com>
2025-08-28 19:32:39 +0530

    First try

---
+++ background.js
@@ -0,0 +1,3 @@
+chrome.action.onClicked.addListener(async tab=>{if(!tab||!tab.id)return;await chrome.scripting.executeScript({target:{tabId:tab.id},files:["contentScript.js"]});await chrome.tabs.sendMessage(tab.id,{t:"TOGGLE"})});
+chrome.runtime.onMessage.addListener((m,s,send)=>{if(m&&m.t==="CAPTURE"){chrome.tabs.captureVisibleTab(s.tab.windowId,{format:"png"},url=>send({ok:true,url}));return true}});
+
